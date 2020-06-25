375fa3f2f063e8429d2f407b2ee783c4a454e984
kot <cadyrov@gmail.com>
2020-06-25 09:42:55 +0500

    init

--- struct.go
+++ struct.go
@@ -1,6 +1,4 @@
-package gauthcl
-
-import "github.com/cadyrov/goerr"
+package godict
 
 type DictionaryRender struct {
 	Id   int    `json:"id"`
@@ -8,29 +6,3 @@ type DictionaryRender struct {
 }
 
 type Dictionary map[string]map[int]string
-
-func (d *Dictionary) DictionaryRender(dictionaryName string, dictionaryId int) (res DictionaryRender, e goerr.IError) {
-	if !d.IsKeyExists(dictionaryName) {
-		e = goerr.New("dictionary_not_found")
-		return
-	}
-	res.Id = dictionaryId
-	res.Name = (*d)[dictionaryName][dictionaryId]
-	return
-}
-
-func (d *Dictionary) IsKeyExists(key string) (res bool) {
-	_, res = (*d)[key]
-	return
-}
-
-func (d *Dictionary) DictionaryIdsInterface(dictionaryName string) (res []interface{}, e goerr.IError) {
-	if !d.IsKeyExists(dictionaryName) {
-		e = goerr.New("dictionary not found")
-		return
-	}
-	for i := range (*d)[dictionaryName] {
-		res = append(res, i)
-	}
-	return
-}
