0b6298079c894e6fbcca2b08a52fe424e707fb1b
kot <cadyrov@gmail.com>
2020-06-25 09:52:10 +0500

    init

--- methods.go
+++ methods.go
@@ -1,6 +1,12 @@
 package godict
 
-import "github.com/cadyrov/goerr"
+import (
+	"encoding/json"
+	"github.com/cadyrov/goerr"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
 
 func (d *Dictionary) DictionaryRender(dictionaryName string, dictionaryId int) (res DictionaryRender, e goerr.IError) {
 	if !d.IsKeyExists(dictionaryName) {
@@ -26,4 +32,66 @@ func (d *Dictionary) DictionaryIdsInterface(dictionaryName string) (res []interf
 		res = append(res, i)
 	}
 	return
+}
+
+func ParseBody(r io.ReadCloser, data interface{}) goerr.IError {
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		return goerr.New(err.Error()).Http(http.StatusBadRequest)
+	}
+	defer func() {
+		err := r.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		return goerr.New(err.Error()).Http(http.StatusBadRequest)
+	}
+	return nil
+}
+
+func Ok(message interface{}, data interface{}, pagination interface{}) *Response {
+	return &Response{HttpCode: http.StatusOK, Message: message, Data: data, Pagination: pagination}
+}
+
+func Error(e goerr.IError) *Response {
+	httpCode := http.StatusInternalServerError
+	if e.GetCode() >= http.StatusBadRequest && e.GetCode() <= http.StatusNetworkAuthenticationRequired {
+		httpCode = e.GetCode()
+	}
+	return &Response{HttpCode: httpCode, Error: e}
+}
+
+func Send(writer http.ResponseWriter, response *Response) {
+	SendJson(writer, response.HttpCode, response)
+}
+
+func SendError(writer http.ResponseWriter, e goerr.IError) {
+	Send(writer, Error(e))
+}
+
+func SendOk(writer http.ResponseWriter, message interface{}, data interface{}, pagination interface{}) {
+	Send(writer, Ok(message, data, pagination))
+}
+
+func SendJson(writer http.ResponseWriter, httpCode int, data interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(httpCode)
+	if data == nil {
+		return
+	}
+	body, err := json.Marshal(data)
+	if err != nil {
+		_, err := writer.Write([]byte(err.Error()))
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
+	_, err = writer.Write(body)
+	if err != nil {
+		panic(err)
+	}
 }
--- struct.go
+++ struct.go
@@ -6,3 +6,18 @@ type DictionaryRender struct {
 }
 
 type Dictionary map[string]map[int]string
+
+// Pagination
+type Pagination struct {
+	Page  int `json:"page,omitempty"`
+	Limit int `json:"limit,omitempty"`
+	Total int `json:"total,omitempty"`
+}
+
+type Response struct {
+	HttpCode   int         `json:"-"`
+	Message    interface{} `json:"message,omitempty"`
+	Data       interface{} `json:"data,omitempty"`
+	Pagination interface{} `json:"pagination,omitempty"`
+	Error      interface{} `json:"error,omitempty"`
+}
